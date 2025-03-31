68d5ea768f6027d5863fee5c6848094c5752be4c
Caden G <165939327+DeliriousCoconut@users.noreply.github.com>
2025-03-31 11:19:09 -0500

    Binary.py with comments

--- Programming Assignments/Program 1/Binary.py
+++ Programming Assignments/Program 1/Binary.py
@@ -1,4 +1,5 @@
 import sys
+# Decode a binary string into readable text
 def crack(text, n):
     res = ""
     cur = 0
@@ -15,10 +16,11 @@ def crack(text, n):
     res += chr(cur)
     return res
 
-
+# Get the input from the user
 file = sys.stdin.read()
 file = file.strip("\n")
 
+# Determine whether the file is 8 or 7 bit then decode
 if len(file) % 8 == 0:
     print("8 bit: " + crack(file, 7))
 
