7a8cb41bc8124495dac2e040a4f5534bb366936c
Drew Ferrington <127445538+DrewBF03@users.noreply.github.com>
2025-03-31 02:47:08 -0500

    Vigenere.py

--- Programming Assignments/Program 2/Vigenere.py
+++ Programming Assignments/Program 2/Vigenere.py
@@ -1,65 +1,75 @@
+# Program 2: Viginere
+# CSC 442: Intro to Cybersecurity - Group 4
+# 3/31/25
+
 import sys
-alphabet = {chr(i): i - ord('a') for i in range(ord('a'), ord('z') + 1)} # dictionary to map letters to numbers (lowercase)
-Alphabet = {chr(i): i - ord('A') for i in range(ord('A'), ord('Z') + 1)} # dictionary to map letters to numbers (uppercase)
+
+def encrypt(text, Key):
+    count = 0  #tracks current position in Key; wraps around when reaching end of key
+    result = ""
+    for letter in text:
+        if letter in alphabet: #checks if lowercase letter (in 'alphabet' dictionary)
+            #convert plaintext to numeric value (0-25), add corresponding number of key letter,
+            #take sum mod26, then convert back to char 
+            ciph = chr(97 + (alphabet[letter] + alphabet[Key[count].lower()]) % 26) 
+            result += ciph #append encrypted character
+            count += 1
+        elif letter in Alphabet: #same process but uppercase
+            ciph = chr(65 + (Alphabet[letter] + alphabet[Key[count].lower()]) % 26) 
+            result += ciph
+            count += 1
+        else: #non-alphabet character; leave it unchanged and do not increment key counter
+            result += letter
+        if count >= len(Key): #reset key counter if necessary
+            count = 0
+    return result
+
+def decrypt(text, Key):
+    count = 0 #tracks current position in Key; wraps around when reaching end of key
+    result = ""
+    for letter in text:
+        if letter in alphabet: #lowercase
+            #decrypt letter using current key character, subtract key value 
+            #from cipher value, add 26 to avoid negatives, take modulo 26,
+            # and convert back to lowercase.
+            plain = chr(97 + (26 + alphabet[letter] - alphabet[Key[count].lower()]) % 26) 
+            result += plain #append plaintext
+            count += 1
+        elif letter in Alphabet: #same process but uppercase
+            plain = chr(65 + (26 + Alphabet[letter] - alphabet[Key[count].lower()]) % 26)
+            result += plain
+            count += 1
+        else: #non-alphabet character; leave it unchanged
+            result += letter
+        if count >= len(Key):
+            count = 0
+    return result
+
+def user(Key): #read user input and direct to either encryption/decryption with Key in tow
+    try:
+        while True:
+            line = input()
+            if sys.argv[1] == "-d":
+                print(decrypt(line, Key))
+            elif sys.argv[1] == "-e":
+                print(encrypt(line, Key))
+    except EOFError:
+        sys.exit(0)
+    except KeyboardInterrupt:
+        sys.exit(0)
 
 def wrong():
     print("Run file using: -e for encryption or -d for decryption followed by Key.")
-    exit()
-    
-def encrypt(text, Key):
-    count = 0
-    res = ""
-    for letter in text:
-        if count >= len(Key):
-            count = 0
-        if letter in alphabet and Key[count].lower() in alphabet:
-            ciph = chr(97 + (alphabet[letter] + alphabet[Key[count].lower()]) % 26)
-        elif letter in Alphabet and Key[count].lower() in alphabet:
-            ciph = chr(65 + (Alphabet[letter] + alphabet[Key[count].lower()]) % 26)
-        else:
-            ciph = letter
-        res += ciph
-        count += 1
-    return res
-
-def decrypt(text, Key):
-    count = 0
-    res = ""
-    for letter in text:
-        if count >= len(Key):
-            count = 0
-        if letter in alphabet and Key[count].lower() in alphabet:
-            plain = chr(97 + (26 + alphabet[letter] - alphabet[Key[count].lower()]) % 26)
-        elif letter in Alphabet and Key[count].lower() in alphabet:
-            plain = chr(65 + (26 + Alphabet[letter] - alphabet[Key[count].lower()]) % 26)
-        else:
-            plain = letter
-        res += plain
-        count += 1
-        
-    return res
-
-def user(Key):
-    while True:
-        try:
-            usrInput = input()
-            if sys.argv[1] == "-d":
-                print(decrypt(usrInput, Key))
-            
-            elif sys.argv[1] == "-e":
-                print(encrypt(usrInput, Key))
-        
-        # Exit program when End of File is reached
-        except EOFError:
-            sys.exit(0)
-                
-        # exit program when interrupted without throwing an error
-        except KeyboardInterrupt:
-            sys.exit(0)
+    sys.exit(0)
 
 
-if len(sys.argv) == 3: # checks that all 3 arguments were provided in command line
-    if sys.argv[1] == "-d" or sys.argv[1] == "-e": # checks for encypt/decrypt
-        user(sys.argv[2]) # the key
+alphabet = {chr(i): i - ord('a') for i in range(ord('a'), ord('z') + 1)} #dictionary to map letters to numbers (lowercase)
+Alphabet = {chr(i): i - ord('A') for i in range(ord('A'), ord('Z') + 1)} #dictionary to map letters to numbers (uppercase)
+if len(sys.argv) == 3: #checks that all 3 arguments were provided in cmd line
+    if sys.argv[1] in ("-d", "-e"): #checks for encypt/decrypt
+        key = "".join(sys.argv[2].split()) #remove spaces from provided key
+        user(key)
     else:
         wrong()
+else:
+    wrong()
