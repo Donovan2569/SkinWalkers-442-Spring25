d5ecb815073698ee5749408e9a2b906bee4ea51e
Donovan Dempster <donovandemp@gmail.com>
2025-05-09 15:43:12 -0500

    Using zoneinfo

--- Programming Assignments/Program 5/TimeLock.py
+++ Programming Assignments/Program 5/TimeLock.py
@@ -1,64 +1,40 @@
-from datetime import datetime, timezone
-import sys
+from datetime import datetime
+from zoneinfo import ZoneInfo
 import hashlib
+import sys
 
-####   ALLOW FOR USER SELECTED TIME   ####
 cur_time = ""
-##########################################
-
-
-####   DAY LIGHT SAVING TIME TOGGLE   ####
-"""
-This may or may not be necessary I am not sure. If it isn't replace
-'DAYLIGHT_SAVINGS'
-in line 59 with 3600 and delete this section. 
-"""
-DST = True
-DAYLIGHT_SAVINGS = 0
-if DST:
-    DAYLIGHT_SAVINGS = 3600
-##########################################
 
 def double_MD5(value):
     value = hashlib.md5(str(value).encode()).hexdigest()
     value = hashlib.md5(str(value).encode()).hexdigest()
-    return value
+
+    return get_code(value)
 
 def get_code(value):
-    ## GET THE FIRST 2 LETTERS FROM LEFT TO RIGHT ##
-    letters = "".join([letter for index, letter in enumerate(value) if letter.isalpha()][:2])
+    letters = "".join([letter for letter in value if letter.isalpha()][:2])
+    num = "".join([num for num in value[::-1] if num.isdigit()][:2])
 
-    ## GET THE FIRST 2 NUMBERS FROM RIGHT TO LEFT ##
-    nums = "".join([num for index, num in enumerate(value[::-1]) if num.isdigit()][:2])
+    #for challenge uncomment 
+    return letters + num # + "".join(value[-1])
 
-    return letters + nums
-
-#### CHECK FOR USER INPUT ####
 if not sys.stdin.isatty():
-    epoch_time = sys.stdin.read().strip()
+    epoch_time = sys.stdin.read().strip('"').split()
+    year, month, day, hour, minute, second = map(int, epoch_time)
+    epoch_time = datetime(year, month, day, hour, minute, second, tzinfo=ZoneInfo("America/Chicago"))
+
 else:
-    epoch_time = "1999 12 31 23 59 59"
+    epoch_time = datetime(1999, 12, 31, 23, 59, 59, tzinfo=ZoneInfo("America/Chicago"))
 
-#### CONVERT EPOCH TIME INTO DATETIME OBJECT ####
-epoch_time = epoch_time.strip('"').split()
-year, month, day, hour, minute, second = map(int, epoch_time)
-epoch_time = datetime(year, month, day, hour, minute, second, tzinfo=timezone.utc)
-
-#### CONVERT CUR TIME INTO DATETIME OBJECT ####
 if cur_time:
-    cur_time = cur_time.strip().split()
+    cur_time = cur_time.strip('"').split()
     year, month, day, hour, minute, second = map(int, cur_time)
-    cur_time = datetime(year, month, day, hour, minute, second, tzinfo=timezone.utc)
+    cur_time = datetime(year, month, day, hour, minute, second, tzinfo=ZoneInfo("America/Chicago"))
 else:
-    cur_time = datetime.now(timezone.utc)
+    cur_time = datetime.now(ZoneInfo("America/Chicago"))
 
-#### CALCULATE ELAPSED TIME IN SECONDS ####
-elapsed = int((cur_time - epoch_time).total_seconds()) - DAYLIGHT_SAVINGS
+elapsed = int((cur_time - epoch_time).total_seconds())
 
-#### USE THE NEAREST MINUTE ####
 elapsed -= elapsed % 60
 
-hash_value = double_MD5(elapsed)
-code = get_code(hash_value)
-sys.stdout.write(code)
-
+print(double_MD5(elapsed), end="\n", flush=True)
